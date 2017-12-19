5c87c0cccf3c71fdba4f0539d53c49f189289336
Miguel <miguelmoutela@hotmail.com>
2017-12-19 16:12:25 +0000

    boiler plate done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 app.use(parser.json());
-app.use(parser.encoded({extended: true}));
+app.use(parser.urlencoded({extended: true}));
 
 app.listen(3000, function(){
   console.log("Listening on port 3000");
